18132b3c68de4df252e74be85a141df96cfcec68
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-08-19 09:26:13 -0600

    Testing update

--- Notes/Test.py
+++ Notes/Test.py
@@ -1,1 +1,5 @@
 print("test")
+print("update")
+testing = input("So like is this working lowkey? You got to like enter something and hit enter.\n")
+testing = testing + "?"
+print("So you entered", testing)
