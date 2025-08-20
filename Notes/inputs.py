39e5be0a84115a45a521012733d410d5958f7a20
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-08-20 09:51:26 -0600

    just an inchident

---
+++ Notes/inputs.py
@@ -0,0 +1,11 @@
+#AP, 1st Period, Input and Outputs Notes 
+
+#variable name = input("Question you're asking/instructions for user")
+name = input("What is your name: ")
+
+print("Hello",name)
+
+
+#Both ways of getting input are fine, as long as you only use one
+print("What is your name: ")
+name = input()
---
+++ Practices/hello_world.py
@@ -0,0 +1,3 @@
+#AP, 1st Period, Hello World
+name = input("Hello! What is your name: ")
+print("Hello", name)
