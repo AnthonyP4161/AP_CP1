43c7e91d2056e3f614943895680b09d8c5023be6
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-09-03 09:30:14 -0600

    idiot proof

---
+++ Practices/idiot_proof.py
@@ -0,0 +1,13 @@
+#AP 1st Period, Idiot Proof Practice
+
+first_name = input("What is your first name: ").capitalize().strip()
+last_name = input("What is your last name: ").capitalize().strip()
+full_name = first_name + " " + last_name
+phone_number = input("What is your phone number: ")
+gpa = float(input("What is your gpa: "))
+gpa = round(gpa,1)
+
+
+print(full_name)
+
+print(gpa)
