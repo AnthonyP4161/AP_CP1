5af6b6cc0744499b7336bdf0322fb237474c674e
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-10-06 09:22:23 -0600

    rock paper scissors

--- Practices/rock_paper_scissors.py
+++ Practices/rock_paper_scissors.py
@@ -1,15 +1,47 @@
 #AP 1st Period, Rock Paper Scissors
 import random
 while True:
-    print("Welcome to rock paper scissors")
-    choice = ("Enter rock, paper, or scissors")
-    if choice != "rock" or "paper" or "scissors":
-        print("Please choose a valid input")
-        continue
+    player_score = 0
+    computer_score = 0
     computer_choice = random.randint(1,3)
-    if computer_choice==1:
-        computer_choice="rock"
-    elif computer_choice==2:
-        computer_choice="paper"
+    print("Welcome to rock paper scissors")
+    choice = input("Enter rock, paper, or scissors, or exit to leave: ").lower().strip()
+    if choice == "rock":
+        if computer_choice == 1:
+            print("Computer chose rock")
+            print("You tied!")
+        elif computer_choice == 2:
+            print("Computer chose paper")
+            print("You won!")
+            player_score += 1
+        else:
+            print("Computer chose scissors")
+            print("You lost!")
+            computer_score += 1
+    elif choice =="paper":
+        if computer_choice == 2:
+            print("Computer chose paper")
+            print("You tied!")
+        elif computer_choice == 1:
+            print("Computer chose rock")
+            print("You won!")
+            player_score += 1
+        else:
+            print("Computer chose scissors")
+            print("You lost!")
+            computer_score += 1
+    elif choice == "scissors":
+        if computer_choice == 3:
+            print("Computer chose scissors")
+            print("You tied!")
+        elif computer_choice == 2:
+            print("Computer chose paper")
+            print("You won!")
+            player_score += 1
+        else:
+            print("Computer chose rock")
+            print("You lost!")
+    elif choice == "exit":
+        break
     else:
-        computer_choice="scissors"
+        print("Please enter something valid")
