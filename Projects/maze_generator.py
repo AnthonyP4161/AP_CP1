947daa7405dbb1173e1706d376441fbf0784d9f0
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-11-03 09:51:35 -0700

    idk

--- Projects/maze_generator.py
+++ Projects/maze_generator.py
@@ -2,30 +2,49 @@
 import turtle
 import random
 
-maze = turtle.Turtle()
-maze.hideturtle()
-maze.speed(5)
-maze.penup()
-maze.goto(300,300)
-maze.pendown()
-maze.goto(50,300)
-maze.penup()
-maze.goto(-50,300)
-maze.pendown()
-maze.goto(-300,300)
-maze.goto(-300,-300)
-maze.goto(-50,-300)
-maze.penup()
-maze.goto(50,-300)
-maze.pendown()
-maze.goto(300,-300)
-maze.goto(300,300)
-for x in range(1,6):
-    ycord = maze.ycor()-100
-    xcord = maze.xcor()+50
+#generate the maze
+def generate():
+    maze = turtle.Turtle()
+    maze.hideturtle()
+    maze.speed(0)
     maze.penup()
-    for x in range(1,6):
-        maze.goto(xcord,ycord)
+    maze.goto(300,300)
     maze.pendown()
-    maze.goto(300,ycord)
+    maze.goto(50,300)
+    maze.penup()
+    maze.goto(-50,300)
+    maze.pendown()
+    maze.goto(-300,300)
+    maze.goto(-300,-300)
+    maze.goto(-50,-300)
+    maze.penup()
+    maze.goto(50,-300)
+    maze.pendown()
+    maze.goto(300,-300)
+    maze.goto(300,300)
+#check if the turtle is at the end
+def is_solvable(row_grid,col_grid):
+    size = len(row_grid)-1
+    visited = set()
+    stack = [(0,0)]
+    while stack:
+        x,y = stack.pop()
+
+        if x == size-1 and y == size-1:
+            return True
+        if (x,y) in visited:
+            continue
+        visited.add((x,y))
+        
+        if x<size-1 and col_grid[y][x+1] == 0:
+            stack.append((x+1,y))
+        if y<size-1 and row_grid[y+1][x] == 0:
+            stack.append((x,y+1))
+        if x>0 and col_grid[y][x] == 0:
+            stack.append((x-1,y))
+        if y>0 and row_grid[y][x] == 0:
+            stack.append((x,y-1))
+    return False
+
+generate()
 turtle.exitonclick()
