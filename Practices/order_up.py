e3ac272cd85c889accbf3e4de5dd886b7499d1fc
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-11-10 09:53:02 -0700

    order up

---
+++ Practices/order_up.py
@@ -0,0 +1,23 @@
+#AP 1st Period, Order Up
+
+main_course = {
+    "burger":5.99,
+    "sandwhich":4.99,
+    "grilled cheese":4.32,
+    "steak":67.67,
+    "eggs":5.49
+}
+drink = {
+    "coke":3.99,
+    "fanta":3.99,
+    "sprite":3.99,
+    "pepsi":3.99
+}
+side_dish = {
+    "mac and cheese":2.99,
+    "mashed potatoes":3.99,
+    "bread sticks":2.99,
+    "fries":3.50,
+    "onion rings":3.99
+}
+
