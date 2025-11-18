9e508af41b36ebf6b8960cbbe62077b3249de4a8
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-11-18 09:52:23 -0700

    idk

--- Practices/flexible_calc.py
+++ Practices/flexible_calc.py
@@ -1,18 +1,18 @@
 #AP 1st Period, Flexible Calculator
-
 def sum(*numbers):
+    total = 0
     for i in numbers:
-        total += numbers
+        total += i
     return total
 def average(*numbers):
+    amount = 0
+    total = 0
     for i in numbers:
-        total += numbers
-        amount = 0
+        total += i
         amount +=1
     return total/amount
 def max(*numbers):
     for i in numbers:
-        max=0
         if i > max:
             max = i
         else:
@@ -29,6 +29,42 @@ def min(*numbers):
 def product(*numbers):
     for i in numbers:
         total = 1
-        total *= numbers
+        total *= i
 
 print("Welcome to the felxible calculator")
+numbers = []
+while True:
+    choice = input("Would you like to calculate the sum, the average, the max, the min, or the product: ").strip().lower()
+    if choice == "sum":
+        while True:
+            number = input("What number would you like to add, type done to be done: ").strip().lower()
+            if number == "done":
+                break
+            elif number.isdigit():
+                numbers.append(float(number))
+            else:
+                print("Enter a valid input")
+                continue
+        print(sum(*numbers))
+    elif choice == "average":
+        while True:
+            number = input("What number would you like to add, type done to be done: ").strip().lower()
+            if number == "done":
+                break
+            elif number.isdigit():
+                numbers.append(float(number))
+            else:
+                print("Enter a valid input")
+                continue
+        print(average(*numbers))
+    elif choice == "max":
+        while True:
+            number = input("What number would you like to add, type done to be done: ").strip().lower()
+            if number == "done":
+                break
+            elif number.isdigit():
+                numbers.append(float(number))
+            else:
+                print("Enter a valid input")
+                continue
+        print(max(*numbers))
