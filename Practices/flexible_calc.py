ec3eea27cdb8c8339c4d6de9f820926a4ed1f1f0
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-11-19 09:27:43 -0700

    flexible calc

--- Practices/flexible_calc.py
+++ Practices/flexible_calc.py
@@ -1,15 +1,18 @@
 #AP 1st Period, Flexible Calculator
+#define the function to get sum
 def sum(*numbers):
     total = 0
     for i in numbers:
         total += i
     return total
+#define the function to get average
 def average(*numbers):
     amount = len(numbers)
     total = 0
     for i in numbers:
         total += i
     return total/amount
+#define the function to get maximum
 def maximum(*numbers):
     max = numbers[1]
     for i in numbers:
@@ -18,6 +21,7 @@ def maximum(*numbers):
         else:
             continue
     return max
+#define the function to get minimum
 def minimum(*numbers):
     min = numbers[1]
     for i in numbers:
@@ -26,16 +30,21 @@ def minimum(*numbers):
         else:
             continue
     return min
+#define the function to get product
 def product(*numbers):
     total = 1
     for i in numbers:
         total *= i
     return total
-
+#welcome the user
 print("Welcome to the felxible calculator")
+#make a list
 numbers = []
+#loop the code
 while True:
+    #ask the user what they want to do
     choice = input("Would you like to calculate the sum, the average, the max, the min, or the product, or type quit to be done: ").strip().lower()
+    #if choice is sum, use the sum function
     if choice == "sum":
         while True:
             number = input("What number would you like to use, type done to be done: ").strip().lower()
@@ -47,6 +56,7 @@ while True:
                 print("Enter a valid input")
                 continue
         print(sum(*numbers))
+    #if choice is average, use the average function
     elif choice == "average":
         while True:
             number = input("What number would you like to use, type done to be done: ").strip().lower()
@@ -58,6 +68,7 @@ while True:
                 print("Enter a valid input")
                 continue
         print(average(*numbers))
+    #if choice is max, use the max function
     elif choice == "max":
         while True:
             number = input("What number would you like to use, type done to be done: ").strip().lower()
@@ -69,6 +80,7 @@ while True:
                 print("Enter a valid input")
                 continue
         print(maximum(*numbers))
+    #if choice is min, use the min function
     elif choice == "min":
         while True:
             number = input("What number would you like to use, type done to be done: ").strip().lower()
@@ -80,6 +92,7 @@ while True:
                 print("Enter a valid input")
                 continue
         print(minimum(*numbers))
+    #if choice is product, use the product function
     elif choice == "product":
         while True:
             number = input("What number would you like to use, type done to be done: ").strip().lower()
@@ -91,8 +104,10 @@ while True:
                 print("Enter a valid input")
                 continue
         print(product(*numbers))
+    #if choice is quit, exit the function
     elif choice == "quit":
         break
+    #if choice isn't valid, make them reenter
     else:
         print("Enter a valid input")
-        continue
+        continue#
