36695f10648985ea2ebe404767e86d0f1da30d78
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-11-24 09:33:36 -0700

    idk

--- Practices/factorial_calc.py
+++ Practices/factorial_calc.py
@@ -14,16 +14,19 @@ def calculate(num):
 #print the instructions for the user
 print("Welcome to the factorial calculator")
 #have the user input something
-num = input("Please input a whole, positive number: ")
-#while the input isn't an integer or a negative sign in the input
-while type(num) != "int" or "-" in num:
-    #have the user input something else
-    try:
-        num = int(input("Please input a whole, positive number: "))
-    except ValueError:
+while True:
+    num = input("Please input a whole, positive number: ")
+    if "-" in num:
+        print("Please input something valid")
         continue
-#make the input an integer
-int(num)
+    else:
+        try:
+            #make the input an integer
+            num = int(num)
+            break
+        except ValueError:
+            print("Please input something valid")
+            continue
 #set a variable and make it equal to the function with input
 total = calculate(num)
 #print the original number and total
--- Practices/tempCodeRunnerFile.py
+++ Practices/tempCodeRunnerFile.py
@@ -1,2 +1,2 @@
 
-            continue
+        num-=1
