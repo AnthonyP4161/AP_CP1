fe7c44e02f561a0d64c884339626908c1a78ff4c
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-11-25 09:52:38 -0700

    idk

---
+++ final/pseudocode.py
@@ -0,0 +1,55 @@
+"""
+ /$$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$$ /$$   /$$  /$$$$$$  /$$$$$$$$
+| $$__  $$ /$$__  $$| $$  /$$/| $$_____/| $$$ | $$ /$$__  $$|__  $$__/
+| $$  \ $$| $$  \ $$| $$ /$$/ | $$      | $$$$| $$| $$  \ $$   | $$   
+| $$$$$$$/| $$  | $$| $$$$$/  | $$$$$   | $$ $$ $$| $$  | $$   | $$   
+| $$____/ | $$  | $$| $$  $$  | $$__/   | $$  $$$$| $$  | $$   | $$   
+| $$      | $$  | $$| $$\  $$ | $$      | $$\  $$$| $$  | $$   | $$   
+| $$      |  $$$$$$/| $$ \  $$| $$$$$$$$| $$ \  $$|  $$$$$$/   | $$   
+|__/       \______/ |__/  \__/|________/|__/  \__/ \______/    |__/  """
+
+
+#a loop for all the code so the user can play again or exit
+    #define the function for the first room
+        #print the description of the room
+        #ask the user which starter they would like to choose and set stats based of which starter
+        #once the user chooses their starter, they will be able to leave the room
+        #return their starter choice and which room they would like to go to
+    #define the function for the second room
+        #print the description of the path
+        #give them the option to look around(though there wont be anything)
+        #let them move to the next area
+    #define the funciton of the town square
+        #print the description of town square
+        #Give them the option to go to any of the buildings or talk to the npcs
+        #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Changing text color in terminal
+                    #BLACK = '\033[30m'
+                    #RED = '\033[31m'
+                    #GREEN = '\033[32m'
+                    #YELLOW = '\033[33m'
+                    #BLUE = '\033[34m'
+                    #MAGENTA = '\033[35m'
+                    #CYAN = '\033[36m'
+                    #WHITE = '\033[37m'
+                    #RESET = '\033[0m' # Resets to default terminal color
+
+                    #print(RED + "This text is red." + RESET)
+                    #print(GREEN + "This text is green." + RESET)
+                    #print(BLUE + "This text is blue." + RESET)
